f0a3bf606262283e8d3a6a5f93c306ce4aa8cd65
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2024-11-19 11:49:39 -0300

    first commit: add react-helmet-async, prettier, eslint and react-router-dom

---
+++ prettier.config.cjs
@@ -0,0 +1,5 @@
+
+
+module.exports = {
+    sortUserPlugins: ['prettier-plugin-tailwindcss']
+}
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from "react-router-dom"
+
+import './index.css'
+import { router } from "./routes"
+import { Helmet, HelmetProvider } from "react-helmet-async"
+
+export function App() {
+
+
+  return (
+  <HelmetProvider>
+    <Helmet titleTemplate="%s | pizza.shop" />
+    <RouterProvider router={router} /> 
+  </HelmetProvider>
+  )
+}
+
+
