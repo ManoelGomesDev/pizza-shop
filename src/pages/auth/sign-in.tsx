28008da74ba0bb859fe9f456d1f1a8760e781042
Manoel Lucio <manoellucio@Manoels-MacBook-Air.local>
2024-11-20 16:25:35 -0300

    finish styles

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,43 +2,50 @@ import { Helmet } from "react-helmet-async";
 import { Label } from "../../components/ui/label";
 import { Input } from "../../components/ui/input";
 import { Button } from "../../components/ui/button";
-import {z} from 'zod'
-import { toast } from 'sonner'
-
+import { z } from "zod";
+import { toast } from "sonner";
+import { Link } from "react-router-dom";
 
 const signInForm = z.object({
-    email: z.string().email()
-})
+  email: z.string().email(),
+});
 
-type SignInForm = z.infer<typeof signInForm>
+type SignInForm = z.infer<typeof signInForm>;
 
-import { useForm } from 'react-hook-form'
+import { useForm } from "react-hook-form";
 
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
 
- const { register, handleSubmit, formState: { isSubmitting }} = useForm<SignInForm>()
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data);
 
- async function handleSignIn(data: SignInForm){
-    try{
-        console.log(data)
-
- await new Promise((resolve) => setTimeout(resolve, 2000))
- toast.success('Enviamos um link de autenticação para seu e-mail', {
-    action: {
-        label: 'Reenviar link',
-        onClick: () => handleSignIn(data)
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de autenticação para seu e-mail", {
+        action: {
+          label: "Reenviar link",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("Crendeciias inválidas");
     }
- })
-
-    }catch(error){
-   toast.error('Crendeciias inválidas')
- }
-}
+  }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant={"ghost"} asChild className="absolute right-4 top-4">
+          <Link to="/sign-up" >
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -49,12 +56,17 @@ export function SignIn() {
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="flex flex-col gap-4 ">
+          <form
+            onSubmit={handleSubmit(handleSignIn)}
+            className="flex flex-col gap-4 "
+          >
             <div>
-                <Label htmlFor="email">Seu e-mail</Label>
-                <Input id="email" type="email" {...register('email')}  />
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register("email")} />
             </div>
-            <Button type="submit" className="w-full" disabled={isSubmitting}  >Acessar painel</Button>
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
